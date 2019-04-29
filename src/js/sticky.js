6d99783e80be0e785fe410d94de29ebf18ecc6a7
maximeRoudier <36051892+maximeRoudier@users.noreply.github.com>
2019-04-29 05:20:33 -0400

    menu + slider + responsive

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -33,7 +33,7 @@ $(document).ready(() => {
         getStickyPosition(){
             $(window).scroll('scroll', () => {
 
-                if($(window).scrollTop() >= ($(this.landing).height() - 40)) {
+                if($(window).scrollTop() >= ($(this.landing).height() - 100)) {
 
 
                     if(this.isStuck == false) {
@@ -69,10 +69,26 @@ $(document).ready(() => {
 
 
     $(".menu-toggle").on('click', function() {
-        $(this).toggleClass("on");
-        $('.menu-section').toggleClass("on");
-        $("nav ul").toggleClass('hidden');
+        $(this).toggleClass('open');
+        $('.menu-section').fadeToggle(600);
     });
+
+		$('.header__menu ul li a').bind('click', function(e) {
+				e.preventDefault(); // prevent hard jump, the default behavior
+
+				var target = $(this).attr("href"); // Set the target as variable
+        $(".menu-toggle").toggleClass('open');
+        $('.menu-section').fadeToggle(600);
+
+				// perform animated scrolling by getting top-position of target-element and set it as scroll target
+				$('html, body').stop().animate({
+						scrollTop: $(target).offset().top
+				}, 1000, function() {
+						location.hash = target; //attach the hash (#jumptarget) to the pageurl
+				});
+
+				return false;
+    		});
 
     const stickyMenu = new Sticky(document.querySelector("body"), document.querySelector('.landing-background'), document.querySelector('.header-content') )
     stickyMenu.init();
--- src/lib/swiper/main.js
+++ src/lib/swiper/main.js
@@ -2,8 +2,7 @@ var appendNumber = 4;
 var prependNumber = 1;
 var swiper = new Swiper('.swiper-container', {
 
-    slidesPerView: 1,
-    centeredSlides: true,
+    slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination',
@@ -13,4 +12,14 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button.next',
         prevEl: '.swiper-button.prev',
     },
+    breakpoints: {
+    720: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+  }
 });
